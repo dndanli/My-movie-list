3c147fcc5af179deae8a1a89eee3140dc5884027
Daniel De Lima <danielkcode@gmail.com>
2022-03-31 23:19:49 -0500

    updated global styling

---
+++ my-movie-list/src/Global.style.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: rgba(255, 255, 255, 0.9);
+    padding-inline:1rem;
+  }
+`;
+
+export default GlobalStyle;
