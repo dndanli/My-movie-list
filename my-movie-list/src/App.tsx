fcf7e90daf8ada793ea59cb428255fd261e23c3b
Daniel De Lima <danielkcode@gmail.com>
2022-03-31 23:27:24 -0500

    updated components in app.tsx

--- my-movie-list/src/App.tsx
+++ my-movie-list/src/App.tsx
@@ -1,5 +1,14 @@
+import GlobalStyle from "./Global.style";
+import StyledNavbar from "./Components/Navbar/Navbar.style";
+import StyledHome from "./Pages/Home.style";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <StyledNavbar className="navbar" />
+      <StyledHome className="home-page" />
+    </div>
+  );
 }
 
 export default App;
