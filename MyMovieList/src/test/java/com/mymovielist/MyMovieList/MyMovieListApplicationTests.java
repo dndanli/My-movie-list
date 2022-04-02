5be9c846c6a228c4842258f4832f4783a19f5e45
Daniel De Lima <danielkcode@gmail.com>
2022-04-02 18:50:14 -0500

    added spring boot setup

---
+++ MyMovieList/src/test/java/com/mymovielist/MyMovieList/MyMovieListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymovielist.MyMovieList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMovieListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
