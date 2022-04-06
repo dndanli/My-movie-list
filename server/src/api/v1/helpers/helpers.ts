11c41b7d9c03f8d071dd837b79964fe9ae355635
Daniel De Lima <danielkcode@gmail.com>
2022-04-06 14:31:03 -0500

    added helpers

---
+++ server/src/api/v1/helpers/helpers.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+import "dotenv/config";
+const apiKey = process.env.API_KEY;
+
+export const fetchPopularMovies = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+    );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const fetchTrending = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/trending/tv/week?api_key=${apiKey}`
+    );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const fetchTrailer = async (id: number) => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/tv/${id}/videos?api_key=${apiKey}&language=en-US`,
+      {
+        headers: {
+          responseType: "application/json",
+        },
+      }
+    );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
