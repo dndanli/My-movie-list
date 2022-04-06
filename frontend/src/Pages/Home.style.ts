15a5b9e30f258ffcbd7f252414aee5118739511c
Daniel De Lima <danielkcode@gmail.com>
2022-04-06 14:34:56 -0500

    moved stylings into separate component

--- frontend/src/Pages/Home.style.ts
+++ frontend/src/Pages/Home.style.ts
@@ -44,35 +44,9 @@ const StyledHome = styled(Home)`
     }
   }
 
-  .trailers-wrapper {
-    display: flex;
-    overflow: auto;
-    .play {
-      margin-right: 1.5rem;
-    }
-    padding-bottom: 2rem;
-  }
-
   #popular-heading {
     color: rgba(16, 16, 16, 0.8);
     margin-bottom: 0.5rem;
-  }
-
-  .player-wrapper {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 2;
-    background-color: black;
-    text-align: end;
-  }
-
-  .close {
-    color: rgba(255, 255, 255, 0.7);
-    margin-right: 0.5rem;
-    font-weight: 700;
-    font-size: 2.4rem;
   }
 `;
 export default StyledHome;
