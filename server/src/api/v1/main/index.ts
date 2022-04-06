48ff8fb5e558cd2890e1df85b4305858cc6ba30e
Daniel De Lima <danielkcode@gmail.com>
2022-04-06 14:36:28 -0500

    added routes to idex.ts

--- server/src/api/v1/main/index.ts
+++ server/src/api/v1/main/index.ts
@@ -1,1 +1,20 @@
-console.log("hello world");
+import express from "express";
+import cors from "cors";
+
+import popularRoutes from "../routes/popular";
+import trendingRoutes from "../routes/trending";
+import getMovieRoutes from "../routes/getMovie";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/popular", popularRoutes);
+app.use("/trending", trendingRoutes);
+app.use("/getMovie", getMovieRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
