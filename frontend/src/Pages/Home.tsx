984611695aa0df755640a34efb3f9edfeb37e987
Daniel De Lima <danielkcode@gmail.com>
2022-04-06 14:34:21 -0500

    moved sections into separate component

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,38 +1,21 @@
 import StyledSearchBar from "../Components/SearchBar/SearchBar.style";
 import StyledCard from "../Components/Card/Card.style";
-import StyledPlay from "../Components/Play/Play.style";
-import ReactPlayer from "react-player/lazy";
+import StyledSection from "../Components/Layout/Section.style";
+
+import { getPopularMovies } from "../Functions/movieDataRequests";
 import { useEffect, useState } from "react";
-
-import testImg from "../Assets/akira.jpg";
-import { IoIosClose } from "react-icons/io";
-
-import {
-  getPopularMovies,
-  getMovieTrailer,
-  getNowPlayingMovies,
-  postMovieId,
-} from "../Functions/GetPopularMovieData";
 
 type Homeprops = {
   className: string;
 };
 
 const Home = ({ className }: Homeprops) => {
-  const [videoPlaying, setVideoPlaying] = useState(false);
   const [popuResponse, setPopResponse] = useState([]);
-  const [nowPlayingResponse, setNowPlaying] = useState([]);
-  const [trailerResponse, setTrailerResponse] = useState([]);
-
-  const displayPlayer = () => setVideoPlaying(!videoPlaying);
 
   useEffect(() => {
     const fetchData = async () => {
       const popularResponseData = await getPopularMovies();
       setPopResponse(popularResponseData.results);
-
-      const nowPlayingResponseData = await getNowPlayingMovies();
-      setNowPlaying(nowPlayingResponseData.results);
     };
     fetchData();
   }, []);
@@ -62,43 +45,7 @@ const Home = ({ className }: Homeprops) => {
           );
         })}
       </div>
-      <h1 id="trending-heading">Trailers</h1>
-      <div className="trailers-wrapper">
-        {nowPlayingResponse.map((data: any) => {
-          return (
-            <StyledPlay
-              className="play"
-              cover={data.poster_path}
-              title={data.title}
-              onClickAction={() => {
-                postMovieId(data.id);
-                displayPlayer();
-              }}
-              key={data.id}
-            />
-          );
-        })}
-      </div>
-      {videoPlaying ? (
-        <div className="player-wrapper">
-          <IoIosClose
-            className="close"
-            onClick={() => {
-              displayPlayer();
-            }}
-          />
-
-          <ReactPlayer
-            controls
-            width="340px"
-            height="280px"
-            url="https://www.youtube.com/watch?v=nA8KmHC2Z-g&ab_channel=RetroBiografen"
-            config={{
-              youtube: { playerVars: { origin: "https://www.youtube.com" } },
-            }}
-          />
-        </div>
-      ) : null}
+      <StyledSection className="section" />
     </div>
   );
 };
