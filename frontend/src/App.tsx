8523aec6921b12e3e452795944c7278b47a33016
Daniel De Lima <danielkcode@gmail.com>
2022-04-08 15:26:05 -0500

    added footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import StyledNavbar from "./Components/Navbar/Navbar.style";
 import StyledHome from "./Pages/Home.style";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StyledDetails from "./Pages/Details.style";
+import StyledFooter from "./Components/Footer/Footer.style";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           ></Route>
         </Routes>
       </BrowserRouter>
+      <StyledFooter className="footer" />
     </div>
   );
 }
