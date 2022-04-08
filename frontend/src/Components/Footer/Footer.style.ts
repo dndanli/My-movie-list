294c17f836fb489f29fce82a7b0689dd6ce326a7
Daniel De Lima <danielkcode@gmail.com>
2022-04-07 23:33:39 -0500

    added footer component styling

---
+++ frontend/src/Components/Footer/Footer.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import Footer from "./Footer";
+
+const StyledFooter = styled(Footer)`
+  font-family: "Inter", sans-serif;
+  font-weight: 500;
+  background-color: black;
+  color: white;
+`;
+
+export default StyledFooter;
