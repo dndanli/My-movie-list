b81ea236220165223282d6d17308bf1d466b2d62
Daniel De Lima <danielkcode@gmail.com>
2022-04-08 15:24:07 -0500

    added header and image to component

--- frontend/src/Components/Footer/Footer.tsx
+++ frontend/src/Components/Footer/Footer.tsx
@@ -1,9 +1,27 @@
+import TMDBLogo from "../../Assets/tmdb.svg";
 type FooterProps = {
   className: string;
 };
 
 const Footer = ({ className }: FooterProps) => {
-  return <div className={className}></div>;
+  return (
+    <div className={className}>
+      <img className="tmdb-logo" src={TMDBLogo} alt="tmdb" />
+      <h4 className="footer-text">
+        Thanks to the
+        <span id="link">
+          <a
+            href="https://www.themoviedb.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TMDB
+          </a>
+        </span>
+        team for providing all of the data on this website.
+      </h4>
+    </div>
+  );
 };
 
 export default Footer;
