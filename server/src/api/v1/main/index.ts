0220e457cd9a3fb8132d528e8856d011e24b2c5a
Daniel De Lima <danielkcode@gmail.com>
2022-04-07 23:37:22 -0500

    added detail router to index.ts

--- server/src/api/v1/main/index.ts
+++ server/src/api/v1/main/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import popularRoutes from "../routes/popular";
 import trendingRoutes from "../routes/trending";
 import getMovieRoutes from "../routes/getMovie";
+import detailRoutes from "../routes/detail";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/popular", popularRoutes);
 app.use("/trending", trendingRoutes);
 app.use("/getMovie", getMovieRoutes);
+app.use("/detail", detailRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
