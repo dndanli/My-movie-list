9387679d48db4e6a99d1dac52ab0025b656ee7ba
Daniel De Lima <danielkcode@gmail.com>
2022-04-07 23:29:05 -0500

    added cast card component styling

---
+++ frontend/src/Components/CastCard/CastCard.style.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import CastCard from "./CastCard";
+
+const StyledCastCard = styled(CastCard)`
+  background-color: rgb(255, 255, 255);
+  min-width: 8rem;
+  max-height: 18rem;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  border-radius: 8px;
+  -moz-box-shadow: 0 0 3px #ccc;
+  -webkit-box-shadow: 0 0 3px #ccc;
+  box-shadow: 0 0 3px #ccc;
+
+  .profile {
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+  }
+  .person-icon {
+    color: rgba(16, 16, 16, 0.3);
+    margin-top: 1.5rem;
+    min-height: 192px;
+    min-width: 128px;
+  }
+
+  .name-wrapper {
+    padding-inline: 0.5rem;
+  }
+  .name {
+    margin-top: 0.5rem;
+    font-size: 1rem;
+    font-family: "Inter", sans-serif;
+    font-weight: 700;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+
+  .character-name {
+    font-size: 13.5px;
+    font-family: "Inter", sans-serif;
+    font-weight: 500;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+`;
+
+export default StyledCastCard;
