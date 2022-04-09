b9862b17d4d23bbfa5bebb6d920ee92f30e96e16
Daniel De Lima <danielkcode@gmail.com>
2022-04-08 19:57:43 -0500

    changed imports and added routes

--- server/src/api/v1/main/index.ts
+++ server/src/api/v1/main/index.ts
@@ -3,8 +3,9 @@ import cors from "cors";
 
 import popularRoutes from "../routes/popular";
 import trendingRoutes from "../routes/trending";
-import getMovieRoutes from "../routes/getMovie";
+import trailerRoutes from "../routes/getMovie";
 import detailRoutes from "../routes/detail";
+import upcomingRoutes from "../routes/upcoming";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -14,8 +15,9 @@ app.use(express.json());
 
 app.use("/popular", popularRoutes);
 app.use("/trending", trendingRoutes);
-app.use("/getMovie", getMovieRoutes);
+app.use("/getIdForTrailer", trailerRoutes);
 app.use("/detail", detailRoutes);
+app.use("/upcoming", upcomingRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
