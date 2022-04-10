fdbe7bda63267a054ec8118ce3a8b7d094227a0e
Daniel De Lima <danielkcode@gmail.com>
2022-04-09 20:50:32 -0500

    updated imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import GlobalStyle from "./Global.style";
 import StyledNavbar from "./Components/Navbar/Navbar.style";
-import StyledHome from "./Pages/Home.style";
+import StyledHome from "./Pages/Home/Home.style";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import StyledDetails from "./Pages/Details.style";
+
 import StyledFooter from "./Components/Footer/Footer.style";
-import StyledCast from "./Pages/Cast.style";
+import StyledCast from "./Pages/Cast/Cast.style";
+import StyledDetails from "./Pages/Details/Details.style";
 
 function App() {
   return (
