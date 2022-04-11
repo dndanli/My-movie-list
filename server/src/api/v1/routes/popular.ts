02228a08a4fe16389aab1fa1210e47abfff81322
Daniel De Lima <danielkcode@gmail.com>
2022-04-11 17:59:48 -0500

    changed route name and added media type

--- server/src/api/v1/routes/popular.ts
+++ server/src/api/v1/routes/popular.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import { fetchPopularMovies } from "../helpers/helpers";
+import { fetchPopularMedia } from "../helpers/helpers";
 const router = express.Router();
 
 /**
  * all routes start with /popular
  */
-router.get("/", async (request, response) => {
-  response.send(await fetchPopularMovies());
+router.get("/:mediaType", async (request, response) => {
+  response.send(await fetchPopularMedia(request.params.mediaType));
 });
 
 export default router;
