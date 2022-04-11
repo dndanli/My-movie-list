d546d523488204c6f784750bac8114185742470d
Daniel De Lima <danielkcode@gmail.com>
2022-04-11 16:52:27 -0500

    added link url

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -22,10 +22,10 @@ const Navbar = ({ className }: NavbarProps) => {
         <div className="mobile-navbar-wrapper">
           {NavbarLinks.map((link) => {
             return (
-              <a href="." className="nav-links-wrapper" key={link.id}>
+              <a href={link.link} className="nav-links-wrapper" key={link.id}>
                 <div className="nav-link-container">
                   <p className="link-icon">{link.linkIcon}</p>
-                  <p>{link.link}</p>
+                  <p>{link.linkName}</p>
                 </div>
               </a>
             );
