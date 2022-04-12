6e96979cbcee81b51ea22768d03f97eda5defed9
Daniel De Lima <danielkcode@gmail.com>
2022-04-12 18:50:13 -0500

    refactored to submit form

--- frontend/src/Components/SearchBar/SearchBar.tsx
+++ frontend/src/Components/SearchBar/SearchBar.tsx
@@ -1,41 +1,35 @@
 import { HiOutlineSearch } from "react-icons/hi";
-import { IoCloseOutline } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+
 import { useState } from "react";
+import axios from "axios";
 
 type SearchBarProps = {
   className: string;
 };
 
 const SearchBar = ({ className }: SearchBarProps) => {
-  const [isCloseIconVis, setCloseIconVis] = useState(false);
-  const [searchInput, setSearchInput] = useState("");
-  const validateInput = (input: string) => !!input.trim();
-  const clearSearchInput = () => {
-    setSearchInput("");
-    setCloseIconVis(false);
+  const [input, setInput] = useState<string>("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios.post(`http://localhost:8000/search/${input}`, { query: input });
+    navigate(`/discover?query=${input}`);
   };
 
   return (
     <div className={className}>
       <HiOutlineSearch className="search-icon" />
-      <input
-        className="search-field"
-        type="search"
-        placeholder="Search"
-        value={searchInput}
-        onChange={(e) => {
-          if (validateInput(e.target.value) === false) {
-            setCloseIconVis(false);
-          } else {
-            setCloseIconVis(true);
-          }
-          setSearchInput(e.target.value);
-        }}
-      ></input>
-      <IoCloseOutline
-        className={isCloseIconVis ? "close-icon-vis" : "close-icon"}
-        onClick={clearSearchInput}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          className="search-field"
+          type="search"
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        ></input>
+      </form>
     </div>
   );
 };
