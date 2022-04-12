614f0e4a4b63f52d28c82c6435329032b4024946
Daniel De Lima <danielkcode@gmail.com>
2022-04-11 20:25:02 -0500

    added color change on click

--- frontend/src/Components/Button/Button.tsx
+++ frontend/src/Components/Button/Button.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 type ButtonProps = {
   className: string;
   text: string;
 };
 
 const Button = ({ className, text }: ButtonProps) => {
+  const [clicked, setCicked] = useState(false);
+
   return (
-    <li className={className}>
+    <li
+      className={className}
+      style={{
+        background:
+          clicked === false ? "rgba(219, 48, 86, 0.7)" : "rgba(219, 48, 86, 1)",
+      }}
+      onClick={() => {
+        setCicked(!clicked);
+      }}
+    >
       <p>{text}</p>
     </li>
   );
