fab7570e15f7cb625d6e27c5c14b63fd269ee106
Daniel De Lima <danielkcode@gmail.com>
2022-04-13 16:45:17 -0500

    added basic validation for the date input

--- frontend/src/Helpers/dateFormatter.ts
+++ frontend/src/Helpers/dateFormatter.ts
@@ -1,6 +1,8 @@
 export const convertToMonthDayYear = (date: string) => {
-  const [year, month, day] = date.split("-");
-  const result = [month, day, year].join("/");
-
-  return result;
+  if (date !== "") {
+    const [year, month, day] = date.split("-");
+    const result = [month, " ", day, ", ", year].join("");
+    return result;
+  }
+  return null;
 };
