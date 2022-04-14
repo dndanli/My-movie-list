f2363cca62cd51681f0494a267a0df4c087604ac
Daniel De Lima <danielkcode@gmail.com>
2022-04-13 20:54:56 -0500

    added PersonProfileBox component

---
+++ frontend/src/Components/PersonProfileBox/PersonProfileBox.tsx
@@ -0,0 +1,41 @@
+import { GoPerson } from "react-icons/go";
+import { Link } from "react-router-dom";
+
+type PersonProfileBoxProps = {
+  className: string;
+  profileImgPath: string;
+  personId: string;
+  personName: string;
+  knownFor: string;
+};
+const PersonProfileBox = ({
+  className,
+  profileImgPath,
+  personId,
+  personName,
+  knownFor,
+}: PersonProfileBoxProps) => {
+  return (
+    <div className={className}>
+      <Link to={`/person${personId}`}>
+        <div className="wrapper">
+          {profileImgPath !== null ? (
+            <img
+              className="profile"
+              src={`https://image.tmdb.org/t/p/w500${profileImgPath}`}
+              alt={personName}
+            />
+          ) : (
+            <GoPerson className="person-icon" />
+          )}
+          <div className="profile-info-wrapper">
+            <h2 className="name">{personName}</h2>
+            <h2 className="knownFor">{knownFor}</h2>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default PersonProfileBox;
