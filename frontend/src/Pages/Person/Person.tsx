c3bdfe1879bbc50bf4c96e95c3cf2ce5c02c1df2
Daniel De Lima <danielkcode@gmail.com>
2022-04-13 21:34:27 -0500

    added fetch for retrieveing person info

--- frontend/src/Pages/Person/Person.tsx
+++ frontend/src/Pages/Person/Person.tsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getPersonResponse } from "../../Helpers/clientHelpers";
+
 type PersonProps = {
   className: string;
 };
 
 const Person = ({ className }: PersonProps) => {
+  const { id } = useParams();
+  const [personResponseData, setPersonResponseData] = useState();
+  useEffect(() => {
+    const fetch = async () => {
+      const personResponse = await getPersonResponse(id);
+      setPersonResponseData(personResponseData);
+      console.log(personResponse);
+    };
+    fetch();
+  }, [id, personResponseData]);
   return <div className={className}></div>;
 };
 
