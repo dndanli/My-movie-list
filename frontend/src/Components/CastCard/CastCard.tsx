87790e9eb68c76693d7268509bf69c2b24ee594c
Daniel De Lima <danielkcode@gmail.com>
2022-04-15 18:13:23 -0500

    added Link to CastCard

--- frontend/src/Components/CastCard/CastCard.tsx
+++ frontend/src/Components/CastCard/CastCard.tsx
@@ -1,4 +1,5 @@
 import { GoPerson } from "react-icons/go";
+import { Link } from "react-router-dom";
 
 type CastCardProps = {
   className: string;
@@ -6,25 +7,30 @@ type CastCardProps = {
     name: string;
     character: string;
     profile_path: string;
+    id: string;
   };
 };
 
 const CastCard = ({ className, person }: CastCardProps) => {
   return (
     <div className={className}>
-      {person.profile_path !== null ? (
-        <img
-          className="profile"
-          src={`https://image.tmdb.org/t/p/w500${person.profile_path}`}
-          alt={person.name}
-        />
-      ) : (
-        <GoPerson className="person-icon" />
-      )}
-      <div className="name-wrapper">
-        <h3 className="name">{person.name}</h3>
-        <h3 className="character-name">{person.character}</h3>
-      </div>
+      <Link to={`/person/${person.id}`}>
+        <div className="wrapper">
+          {person.profile_path !== null ? (
+            <img
+              className="profile"
+              src={`https://image.tmdb.org/t/p/w500${person.profile_path}`}
+              alt={person.name}
+            />
+          ) : (
+            <GoPerson className="person-icon" />
+          )}
+          <div className="name-wrapper">
+            <h3 className="name">{person.name}</h3>
+            <h3 className="character-name">{person.character}</h3>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
