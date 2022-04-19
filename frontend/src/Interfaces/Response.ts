a7c642c4be559ca17f581136f4e695e0ccef9417
Daniel De Lima <danielkcode@gmail.com>
2022-04-19 14:56:05 -0500

    added reponse.ts interface

---
+++ frontend/src/Interfaces/Response.ts
@@ -0,0 +1,17 @@
+export interface Response {
+  0: {
+    results: [];
+    total_results: number;
+    total_pages: number;
+  };
+  1: {
+    results: [];
+    total_results: number;
+    total_pages: number;
+  };
+  2: {
+    results: [];
+    total_results: number;
+    total_pages: number;
+  };
+}
