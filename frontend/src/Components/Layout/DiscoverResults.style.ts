7a2fc7e8008c73077077690bed30c910255b42a3
Daniel De Lima <danielkcode@gmail.com>
2022-04-19 16:06:13 -0500

    added react paginate styling

---
+++ frontend/src/Components/Layout/DiscoverResults.style.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  .discover-paginate{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    list-style-type: none;
+    font-family:"Inter", sans-serif;
+    font-weight: 500;
+    color:rgba(16, 16, 16, 0.7);
+  }
+  .previous-btn, .next-btn{
+    font-size: 1.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .paginate-active{
+    color: white;
+    background-color: rgba(219, 48, 86, 1);
+    padding-inline: 1rem;
+    text-align: center;
+    border-radius: 2px;
+    transition: 200ms ease-in;
+  }
+
+`;
