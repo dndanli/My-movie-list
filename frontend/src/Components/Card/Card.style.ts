ac1fdca970e6a4a8587b3cc458c2dc105d8a00dd
Daniel De Lima <danielkcode@gmail.com>
2022-04-20 14:48:40 -0500

    styling update

--- frontend/src/Components/Card/Card.style.ts
+++ frontend/src/Components/Card/Card.style.ts
@@ -10,9 +10,12 @@ const StyledCard = styled(Card)`
     width: 10.3rem;
     min-height: 21rem;
     border-radius: 11px;
-    background-color: rgba(255, 255, 255, 1);
+    background-color: rgba(255, 255, 255, 0.9);
     position: relative;
     padding-bottom: 0.5rem;
+    -moz-box-shadow: 0 0 0.5px #ccc;
+    -webkit-box-shadow: 0 0 0.5px #ccc;
+    box-shadow:0 0 0 0.5px #ccc;
   }
 
   a {
@@ -44,7 +47,7 @@ const StyledCard = styled(Card)`
     padding-left: 1rem;
     font-size: 1rem;
     font-weight: 700;
-    color: rgba(16, 16, 16, 0.8);
+    color:rgba(16,16,16, 0.9);
     p {
       text-overflow: ellipsis;
       overflow: hidden;
@@ -53,14 +56,14 @@ const StyledCard = styled(Card)`
   }
 
   .rating-score {
-    min-width: 3.3rem;
+    min-width: 4rem;
     min-height: 2rem;
     font-weight: 700;
     display: flex;
     justify-content: space-evenly;
     align-items: center;
     border-radius: 15px;
-    background-color: rgb(219, 48, 86);
+    background-color:rgb(255, 140, 50);
     color: rgba(255, 255, 255, 1);
     position: absolute;
     bottom: 6.8rem;
@@ -73,7 +76,7 @@ const StyledCard = styled(Card)`
 
   .date {
     padding-inline: 1rem;
-    color: rgba(16, 16, 16, 0.8);
+    color: rgba(16, 16, 16, 1);
   }
 
   .icon {
