127c5cc23b77cf6203b9adc0959ffbe39112a176
Daniel De Lima <danielkcode@gmail.com>
2022-04-20 14:41:29 -0500

    added sign up page

---
+++ frontend/src/Pages/SignUp/SignUp.tsx
@@ -0,0 +1,38 @@
+type SignUpProps = {
+  className: string;
+};
+const SignUp = ({ className }: SignUpProps) => {
+  return (
+    <div className={className}>
+      <h1 className="header">Sign Up</h1>
+
+      <form className="form-container">
+        <div className="input-container">
+          <label htmlFor="username" className="input-label">
+            Username
+          </label>
+          <input type="text" name="username" className="user-input" />
+        </div>
+
+        <div className="input-container">
+          <label htmlFor="email" className="input-label">
+            Email
+          </label>
+          <input type="text" name="email" placeholder="example@email.com" className="user-input" />
+        </div>
+
+        <div className="input-container">
+          <label htmlFor="password" className="input-label">
+            Password
+          </label>
+          <input type="password" name="password" className="user-input" />
+        </div>
+        <div className="submit-wrapper">
+          <input type="submit" value={"Create an Account"} className="submit-btn" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
