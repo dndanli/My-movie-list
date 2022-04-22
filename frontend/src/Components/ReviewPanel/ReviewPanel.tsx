b138b9f001240d2e8039ac191e543622b42d7ea4
Daniel De Lima <danielkcode@gmail.com>
2022-04-22 16:29:33 -0500

    added date

--- frontend/src/Components/ReviewPanel/ReviewPanel.tsx
+++ frontend/src/Components/ReviewPanel/ReviewPanel.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { AiFillStar } from "react-icons/ai";
-import { useState, ImgHTMLAttributes } from "react";
+import { convertISO8601toMMDDYYY } from "../../Helpers/dateFormatter";
 
 type ReviewsProps = {
   className: string;
@@ -9,6 +9,7 @@ type ReviewsProps = {
   authorReview: string;
   authorRating: number;
   authorProfile: string;
+  dateCreated: string;
 };
 
 const ReviewPanel = ({
@@ -18,6 +19,7 @@ const ReviewPanel = ({
   authorReview,
   authorRating,
   authorProfile,
+  dateCreated,
 }: ReviewsProps) => {
   return (
     <div className={className}>
@@ -33,20 +35,23 @@ const ReviewPanel = ({
             {" "}
             read all
           </Link>
+          <p className="posted">posted on {convertISO8601toMMDDYYY(dateCreated)}</p>
         </div>
       </div>
       <div className="user-wrapper">
-        <img
-          src={`${authorProfile.slice(1)}`}
-          alt={authorName}
-          onError={(e) => {
-            e.currentTarget.src = `https://image.tmdb.org/t/p/w400${authorProfile}`;
-          }}
-          className="user"
-        />
+        <div className="profile-img-wrapper">
+          <img
+            src={`${authorProfile.slice(1)}`}
+            alt={authorName}
+            onError={(e) => {
+              e.currentTarget.src = `https://image.tmdb.org/t/p/w400${authorProfile}`;
+            }}
+            className="user"
+          />
+        </div>
         <div className="name-wrapper">
           <h4 className="author-name">{authorName}</h4>
-          <h4 className="author-name">@{authorUsername}</h4>
+          <h4 className="author-username">@{authorUsername}</h4>
         </div>
       </div>
     </div>
