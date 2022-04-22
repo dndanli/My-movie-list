a2ec54fff3c24d620e5d4e5211c993aca6d9bad2
Daniel De Lima <danielkcode@gmail.com>
2022-04-22 16:29:14 -0500

    added convertISO8601toMMDDYYY function

--- frontend/src/Helpers/dateFormatter.ts
+++ frontend/src/Helpers/dateFormatter.ts
@@ -6,3 +6,12 @@ export const convertToMonthDayYear = (date: string) => {
   }
   return null;
 };
+
+export const convertISO8601toMMDDYYY = (dateInput: string) => {
+  const date = new Date(dateInput);
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const formattedMonth = date.toLocaleString("default", { month: "long" });
+
+ return `${formattedMonth} ${day} ${year}`;
+};
