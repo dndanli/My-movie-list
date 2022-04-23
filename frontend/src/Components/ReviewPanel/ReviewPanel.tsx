3ec7cf80b8bff5eba0470b980baea4964dda67a0
Daniel De Lima <danielkcode@gmail.com>
2022-04-23 16:12:39 -0500

    show tmdb user icon and show user icon if no avatar is available

--- frontend/src/Components/ReviewPanel/ReviewPanel.tsx
+++ frontend/src/Components/ReviewPanel/ReviewPanel.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import { AiFillStar } from "react-icons/ai";
-import {BsFillPersonFill} from "react-icons/bs";
+import { BsFillPersonFill } from "react-icons/bs";
 import { convertISO8601toMMDDYYY } from "../../Helpers/dateFormatter";
-import {useState} from "react";
+import { useState } from "react";
 
 type ReviewsProps = {
   className: string;
@@ -12,6 +12,7 @@ type ReviewsProps = {
   authorRating: number;
   authorProfile: string;
   dateCreated: string;
+  tmdbUser: boolean;
 };
 
 const ReviewPanel = ({
@@ -22,17 +23,21 @@ const ReviewPanel = ({
   authorRating,
   authorProfile,
   dateCreated,
+  tmdbUser,
 }: ReviewsProps) => {
-
   const [imageLoadError, setImageLoadError] = useState(true);
 
   return (
     <div className={className}>
       <div className="wrapper">
+      {
+        authorRating !== null ?
         <div className="rating">
           <p>{authorRating}</p>
           <AiFillStar className="star-icon" />
         </div>
+        :null
+      }
 
         <div className="content">
           <p>{authorReview}</p>
@@ -40,30 +45,33 @@ const ReviewPanel = ({
             {" "}
             read all
           </Link>
-          <p className="posted">posted on {convertISO8601toMMDDYYY(dateCreated)}</p>
+          <p className="posted">
+            posted on {convertISO8601toMMDDYYY(dateCreated)}
+          </p>
         </div>
       </div>
       <div className="user-wrapper">
         <div className="profile-img-wrapper">
-        {
-        authorProfile !== null  ?
-        <img
-          src={ authorProfile ? `${authorProfile.slice(1)}` : ""}
-            alt={authorName}
-            onError={(e)=>{
-              if(imageLoadError){
-                setImageLoadError(false);
-                e.currentTarget.src = `https://image.tmdb.org/t/p/w400${authorProfile}`;
-              }
-            }}
-            className="user"
-          />
-          :<BsFillPersonFill className="user-icon"/>
-        }
+          {authorProfile !== null ? (
+            <img
+              src={authorProfile ? `${authorProfile.slice(1)}` : ""}
+              alt={authorName}
+              onError={(e) => {
+                if (imageLoadError) {
+                  setImageLoadError(false);
+                  e.currentTarget.src = `https://image.tmdb.org/t/p/w400${authorProfile}`;
+                }
+              }}
+              className="user"
+            />
+          ) : (
+            <BsFillPersonFill className="user-icon" />
+          )}
         </div>
         <div className="name-wrapper">
           <h4 className="author-name">{authorName}</h4>
           <h4 className="author-username">@{authorUsername}</h4>
+          {tmdbUser ? <div className="tmdb-user">TMDB</div> : null}
         </div>
       </div>
     </div>
