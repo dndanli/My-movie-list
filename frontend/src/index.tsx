00260a76a0ae4b6076065b838926c96dbfdacef2
Daniel De Lima <danielkcode@gmail.com>
2022-04-24 15:16:49 -0500

    downgrade back to react 17

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,11 @@
-import * as ReactDOMClient from "react-dom/client";
+import React from "react";
+import ReactDOM from 'react-dom';
+
 import App from "./App";
 
-const container = document.getElementById("root") as Element;
-
-// Create a root.
-const root = ReactDOMClient.createRoot(container);
-
-// Initial render: Render an element to the root.
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
