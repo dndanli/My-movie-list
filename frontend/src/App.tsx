2bafd516a771aafb581e3c28b1f959363b78d98f
Daniel De Lima <danielkcode@gmail.com>
2022-04-24 01:10:00 -0500

    addded Wrapper

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,64 +1,66 @@
-import GlobalStyle from "./Global.style";
-import StyledNavbar from "./Components/Navbar/Navbar.style";
-import StyledHome from "./Pages/Home/Home.style";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import StyledFooter from "./Components/Footer/Footer.style";
+import { GlobalStyle, Wrapper } from "./Global.style";
+import StyledNavbar from "./Components/Navbar/Navbar.style";
+import StyledHome from "./Pages/Home/Home.style";
 import StyledCast from "./Pages/Cast/Cast.style";
 import StyledDetails from "./Pages/Details/Details.style";
 import StyledDiscover from "./Pages/Discover/Discover.style";
 import StyledPerson from "./Pages/Person/Person.style";
 import StyledSignUp from "./Pages/SignUp/SignUp.style";
 import StyledLogin from "./Pages/Login/Login.style";
-
+import StyledSidebar from "./Components/Sidebar/Sidebar.style";
+import StyledContentBar from "./Components/ContentBar/ContentBar.style";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
-      <BrowserRouter>
-        <StyledNavbar className="navbar" />
-        <Routes>
-          <Route
-            path="/"
-            element={<StyledHome className="home-page" />}
-          ></Route>
-          <Route
-            path="/discover"
-            element={<StyledDiscover className="discover-page" />}
-          ></Route>
-          <Route
-            path="/discover:query"
-            element={<StyledDiscover className="discover-page" />}
-          ></Route>
-          <Route
-            path="/discover/:page/:query"
-            element={<StyledDiscover className="discover-page" />}
-          ></Route>
-          <Route
-            path="detail/:mediaType/:id"
-            element={<StyledDetails className="detail-page" />}
-          ></Route>
-          <Route
-            path="/:category/cast/:mediaType/:id"
-            element={<StyledCast className="cast-page" />}
-          ></Route>
-          <Route
-            path="/person/:id"
-            element={<StyledPerson className="person-page" />}
-          ></Route>
-          <Route
-            path="/signup"
-            element={<StyledSignUp className="signup-page" />}
-          ></Route>
-          <Route
-            path="/login"
-            element={<StyledLogin className="login-page" />}
-          ></Route>
-        </Routes>
-      </BrowserRouter>
-
-      <StyledFooter className="footer" />
+      <Wrapper>
+        <GlobalStyle />
+        <BrowserRouter>
+          <StyledSidebar className="sidebar" />
+          <StyledNavbar className="navbar" />
+          <Routes>
+            <Route
+              path="/"
+              element={<StyledHome className="home-page" />}
+            ></Route>
+            <Route
+              path="/discover"
+              element={<StyledDiscover className="discover-page" />}
+            ></Route>
+            <Route
+              path="/discover:query"
+              element={<StyledDiscover className="discover-page" />}
+            ></Route>
+            <Route
+              path="/discover/:page/:query"
+              element={<StyledDiscover className="discover-page" />}
+            ></Route>
+            <Route
+              path="detail/:mediaType/:id"
+              element={<StyledDetails className="detail-page" />}
+            ></Route>
+            <Route
+              path="/:category/cast/:mediaType/:id"
+              element={<StyledCast className="cast-page" />}
+            ></Route>
+            <Route
+              path="/person/:id"
+              element={<StyledPerson className="person-page" />}
+            ></Route>
+            <Route
+              path="/signup"
+              element={<StyledSignUp className="signup-page" />}
+            ></Route>
+            <Route
+              path="/login"
+              element={<StyledLogin className="login-page" />}
+            ></Route>
+          </Routes>
+          <StyledContentBar className="content-bar" />
+        </BrowserRouter>
+      </Wrapper>
     </div>
   );
 }
