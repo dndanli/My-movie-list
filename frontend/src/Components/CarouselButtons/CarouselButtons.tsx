22cdbb055d58dea326228509fecf878d7e4f5db2
Daniel De Lima <danielkcode@gmail.com>
2022-04-24 22:44:47 -0500

    added carousel buttons

---
+++ frontend/src/Components/CarouselButtons/CarouselButtons.tsx
@@ -0,0 +1,21 @@
+import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
+type ButtonProps = {
+  className: string;
+  onClick: any;
+};
+
+export const PrevButton = ({ className, onClick }: ButtonProps) => {
+  return (
+    <div className={className} onClick={onClick}> 
+      <MdArrowBackIos />
+    </div>
+  );
+};
+
+export const NextButton = ({ className, onClick }: ButtonProps) => {
+  return (
+    <div className={className} onClick={onClick}>
+      <MdArrowForwardIos />
+    </div>
+  );
+};
