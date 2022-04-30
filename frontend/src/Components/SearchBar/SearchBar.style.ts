feb3fa9a7f72482e9c3754b1d78ac002b2788e96
Daniel De Lima <danielkcode@gmail.com>
2022-04-30 13:32:35 -0500

    search bar ui update

--- frontend/src/Components/SearchBar/SearchBar.style.ts
+++ frontend/src/Components/SearchBar/SearchBar.style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import SearchBar from "./SearchBar";
 
 const StyledSearchBar = styled(SearchBar)`
-  width: 20rem;
+  width: 100%;
   background-color: white;
   min-height: 2rem;
   padding-block: 0.5rem;
@@ -10,7 +10,8 @@ const StyledSearchBar = styled(SearchBar)`
   background-color: rgba(255, 255, 255, 1);
   display: flex;
   align-items: center;
-  box-shadow:0 0 0 0.5px #ccc;
+  box-shadow: 0 0 0 0.5px #ccc;
+  font-family: "Poppins", sans-serif;
   .search-icon {
     font-size: 2rem;
     color: rgba(16, 16, 16, 0.7);
@@ -19,31 +20,32 @@ const StyledSearchBar = styled(SearchBar)`
 
   .close-icon {
     font-size: 1.5rem;
-    padding-left: 10px;
+    margin-left: auto;
+    margin-right: 1rem;
     color: rgba(16, 16, 16, 0.7);
   }
 
   display: flex;
+  .form-container {
+    width: 90%;
+  }
+
   .search-field {
-    width: 100%;
-    max-width: 100%;
-    min-width: 20px;
     outline: none;
     border: none;
     font-size: 16px;
     padding-left: 10px;
     padding-right: 10px;
-  }
-
-  input.search-field {
     background-color: rgba(255, 255, 255, 0.9);
+    /* background-color: rebeccapurple; */
     min-height: 2rem;
+    width: 90%;
   }
 
-  input[type="search"]::-webkit-search-decoration,
-  input[type="search"]::-webkit-search-cancel-button,
-  input[type="search"]::-webkit-search-results-button,
-  input[type="search"]::-webkit-search-results-decoration {
+  input[type="text"]::-webkit-search-decoration,
+  input[type="text"]::-webkit-search-cancel-button,
+  input[type="text"]::-webkit-search-results-button,
+  input[type="text"]::-webkit-search-results-decoration {
     -webkit-appearance: none;
   }
 `;
