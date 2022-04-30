3476023191a2800275403aa6a60e0bfae454a709
Daniel De Lima <danielkcode@gmail.com>
2022-04-30 13:35:20 -0500

    added cast carousel component

---
+++ frontend/src/Components/CastCarousel/CastCarousel.style.ts
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import CastCarousel from "./CastCarousel";
+
+const StyledCastCarousel = styled(CastCarousel)`
+  overflow: hidden;
+  position: relative;
+  margin-top: 2rem;
+  padding-top: 3.5rem;
+  padding-bottom: 3rem;
+  padding-left: 0.5rem;
+
+  .embla__container {
+    display: flex;
+
+    /* display: grid; */
+    /* grid-template-columns: repeat(5, 1fr); */
+  }
+  .heading {
+    color: rgba(255, 255, 255, 0.9);
+    position: absolute;
+    top: 1%;
+    font-size: 1rem;
+    font-weight: 500;
+  }
+  .embla__button {
+    color: rgba(255, 255, 255, 0.9);
+    position: absolute;
+    transition: 500ms ease-in-out;
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+    font-size: 1.2rem;
+    top: 0;
+    padding: 0.2rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border: solid 1px #ccc;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  .prev {
+    right: 4rem;
+  }
+
+  .next {
+    right: 0;
+  }
+
+  .heading {
+    color: rgba(255, 255, 255, 0.9);
+    position: absolute;
+    top: 1%;
+    font-size: 1.7rem;
+  }
+
+  .card {
+    margin-right: 1.5rem;
+  }
+`;
+
+export default StyledCastCarousel;
---
+++ frontend/src/Components/CastCarousel/CastCarousel.tsx
@@ -0,0 +1,59 @@
+import useEmblaCarousel from "embla-carousel-react";
+import { useCallback, useEffect } from "react";
+import { NextButton, PrevButton } from "../CarouselButtons/CarouselButtons";
+import StyledCastCard from "../CastCard/CastCard.style";
+import { v4 as uuidv4 } from "uuid";
+
+type CastCarouselProps = {
+  className: string;
+  slides: [];
+  total: number;
+  heading: string;
+};
+
+const CastCarousel = ({
+  className,
+  slides,
+  total,
+  heading,
+}: CastCarouselProps) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    dragFree: true,
+    containScroll: "trimSnaps",
+    slidesToScroll: 2,
+    speed: 25,
+  });
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  useEffect(() => {
+    if (emblaApi) emblaApi.reInit();
+  }, [emblaApi, slides]);
+
+  return (
+    <div className={className} ref={emblaRef}>
+      <div className="embla__container">
+        {slides.slice(0, total).map((person: any) => {
+          return (
+            <StyledCastCard
+              key={uuidv4()}
+              className="cast-card"
+              person={person}
+            />
+          );
+        })}
+      </div>
+      <h2 className="heading">{heading}</h2>
+      <PrevButton className="embla__button prev" onClick={scrollPrev} />
+      <NextButton className="embla__button next" onClick={scrollNext} />
+    </div>
+  );
+};
+
+export default CastCarousel;
