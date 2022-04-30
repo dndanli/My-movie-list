6b829fcd811028dd6006f2ac7c22ffae7627f248
Daniel De Lima <danielkcode@gmail.com>
2022-04-30 13:28:09 -0500

    ui update

--- frontend/src/Components/Button/Button.style.ts
+++ frontend/src/Components/Button/Button.style.ts
@@ -2,11 +2,15 @@ import styled from "styled-components";
 import Button from "./Button";
 
 const StyledButton = styled(Button)`
+  font-family: "Poppins", sans-serif;
   font-weight: 500;
   font-size: 14px;
   max-height: 1.2rem;
   white-space: nowrap;
   display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
   color: rgba(255, 255, 255, 0.9);
   border-radius: 5px;
   padding-block: 0.5rem;
