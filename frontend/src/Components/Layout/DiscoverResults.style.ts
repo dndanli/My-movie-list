308bf8e53237c8a3eb039e345d3a6cff70041460
Daniel De Lima <danielkcode@gmail.com>
2022-04-30 13:30:54 -0500

    discover results ui update

--- frontend/src/Components/Layout/DiscoverResults.style.ts
+++ frontend/src/Components/Layout/DiscoverResults.style.ts
@@ -3,24 +3,33 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   .discover-paginate {
     display: flex;
-    justify-content: space-evenly;
     align-items: center;
+    justify-content: space-around;
     list-style-type: none;
     font-family: "Inter", sans-serif;
     font-weight: 500;
-    color: rgba(16, 16, 16, 0.7);
+    color: rgba(255, 255, 255, 0.7);
+    li {
+      a {
+        cursor: pointer;
+      }
+    }
   }
+
   .previous-btn,
   .next-btn {
     font-size: 1.5rem;
+    font-weight: 500;
     display: flex;
     justify-content: center;
     align-items: center;
+    color: rgba(255, 255, 255, 0.7);
+    cursor: pointer;
   }
 
   .paginate-active {
     color: white;
-    background-color: rgb(218, 0, 55);
+    background-color: rgb(255, 140, 50);
     padding-inline: 1rem;
     text-align: center;
     border-radius: 2px;
