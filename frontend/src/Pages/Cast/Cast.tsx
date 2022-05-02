7c5d6090c54c78f3b2e251897ea7a576d4f1bbeb
Daniel De Lima <danielkcode@gmail.com>
2022-05-01 20:13:55 -0500

    cast page refactor

--- frontend/src/Pages/Cast/Cast.tsx
+++ frontend/src/Pages/Cast/Cast.tsx
@@ -1,8 +1,8 @@
-import { GoPerson } from "react-icons/go";
-
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getMediaDetails } from "../../Helpers/clientHelpers";
+import StyledCastListItem from "../../Components/CastListItem/CastListItem.style";
+import { v4 as uuidv4 } from "uuid";
 
 type CastProps = {
   className: string;
@@ -25,53 +25,32 @@ const Cast = ({ className }: CastProps) => {
   return (
     <div className={className}>
       {castData.length > 0 ? <h2 className="cast-header">Cast</h2> : null}
-      <ul>
-        {castData.map((data: any, index) => {
+      <ul className="cast-info-desktop">
+        {castData.map((data: any) => {
           return (
-            // TODO: replace index with uid
-            <Link to={`/person/${data.id}`} key={index}>
-              <li className="list-item">
-                {data.profile_path !== null ? (
-                  <img
-                    className="profile"
-                    src={`https://image.tmdb.org/t/p/w500${data.profile_path}`}
-                    alt={data.name}
-                  />
-                ) : (
-                  <GoPerson className="person-icon" />
-                )}
-                <div className="name-wrapper">
-                  <h4 className="name">{data.name}</h4>
-                  <h4 className="role">{data.character}</h4>
-                </div>
-              </li>
-            </Link>
+            <StyledCastListItem
+              className="cast-list-item"
+              id={data.id}
+              profilePath={data.profile_path}
+              _name={data.name}
+              character={data.character}
+              key={uuidv4()}
+            />
           );
         })}
       </ul>
       {crewData.length > 0 ? <h2 className="cast-header">Crew</h2> : null}
-      
       <ul>
-        {crewData.map((data: any, index) => {
+        {crewData.map((data: any) => {
           return (
-            // TODO: replace index with uid
-            <Link to={`/person/${data.id}`} key={index}>
-              <li key={index} className="list-item">
-                {data.profile_path !== null ? (
-                  <img
-                    className="profile"
-                    src={`https://image.tmdb.org/t/p/w500${data.profile_path}`}
-                    alt={data.name}
-                  />
-                ) : (
-                  <GoPerson className="person-icon" />
-                )}
-                <div className="name-wrapper">
-                  <h4 className="name">{data.name}</h4>
-                  <h4 className="role">{data.job}</h4>
-                </div>
-              </li>
-            </Link>
+            <StyledCastListItem
+              className="cast-list-item"
+              id={data.id}
+              profilePath={data.profile_path}
+              _name={data.name}
+              character={data.job}
+              key={uuidv4()}
+            />
           );
         })}
       </ul>
