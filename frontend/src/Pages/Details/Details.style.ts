5a7b027f05969094a4bed1469bc71321119ce17b
Daniel De Lima <danielkcode@gmail.com>
2022-05-01 20:15:18 -0500

    removed css

--- frontend/src/Pages/Details/Details.style.ts
+++ frontend/src/Pages/Details/Details.style.ts
@@ -7,9 +7,6 @@ const StyledDetails = styled(Details)`
   margin-bottom: 2rem;
   padding-inline: 1rem;
 
-  .desktop-view-wrapper {
-    display: none;
-  }
   .overview-title-header,
   .cast-info,
   #overview-info-header,
@@ -93,42 +90,6 @@ const StyledDetails = styled(Details)`
 
   a {
     text-decoration: none;
-  }
-  .list-item {
-    border-radius: 6px;
-    background-color: rgba(255, 255, 255, 0.9);
-    margin-bottom: 1rem;
-    display: flex;
-    -moz-box-shadow: 0 0 0.5px #ccc;
-    -webkit-box-shadow: 0 0 0.5px #ccc;
-    box-shadow: 0 0 0 0.5px #ccc;
-  }
-
-  .profile {
-    border-top-left-radius: 6px;
-    border-bottom-left-radius: 6px;
-    object-fit: cover;
-    min-width: 100px;
-    max-height: 120px;
-  }
-
-  .person-icon {
-    min-width: 100px;
-    min-height: 120px;
-    color: rgba(16, 16, 16, 0.5);
-  }
-
-  .name-wrapper {
-    padding-inline: 0.7rem;
-    color: rgba(255, 255, 255, 0.9);
-  }
-  .name,
-  .role {
-    font-family: "Inter", sans-serif;
-    font-weight: 500;
-    font-size: 16px;
-    margin-top: 1rem;
-    color: rgba(16, 16, 16, 0.8);
   }
 
   /* 535px */
