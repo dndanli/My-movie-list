d49624bcdc6fb43a14f9ad93a610ea2b26d278d3
Daniel De Lima <danielkcode@gmail.com>
2022-05-01 20:10:05 -0500

    created cast list item component

---
+++ frontend/src/Components/CastListItem/CastListItem.style.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import CastListItem from "./CastListItem";
+
+const StyledCastListItem = styled(CastListItem)` 
+  .list-item {
+    border-radius: 6px;
+    background-color: rgb(34, 35, 46);
+    margin-bottom: 1rem;
+    display: flex;
+    -moz-box-shadow: 0 0 0.5px #ccc;
+    -webkit-box-shadow: 0 0 0.5px #ccc;
+    box-shadow: 0 0 0 0.5px #ccc;
+  }
+  a {
+    text-decoration: none;
+    .name {
+      color: rgba(255, 255, 255, 0.9);
+    }
+    .character-name {
+      color: rgba(255, 255, 255, 0.8);
+    }
+  }
+
+  .profile {
+    border-top-left-radius: 6px;
+    border-bottom-left-radius: 6px;
+    object-fit: cover;
+    min-width: 100px;
+    max-height: 120px;
+  }
+
+  .person-icon {
+    min-width: 100px;
+    min-height: 120px;
+    color: rgba(255, 255, 255, 0.5);
+  }
+
+  .name-wrapper {
+    padding-inline: 0.7rem;
+  }
+  .name{
+    font-family: "Poppins", sans-serif;
+    font-weight: 700;
+    font-size: 1.1rem;
+    margin-top: 1rem;
+    color: rgba(255, 255, 255, 0.9);
+
+  }
+  .role {
+    font-family: "Poppins", sans-serif;
+    font-weight: 500;
+    font-size: 16px;
+    margin-top: 1rem;
+    color: rgba(255, 255, 255, 0.7);
+  }
+
+`;
+
+
+export default StyledCastListItem;
---
+++ frontend/src/Components/CastListItem/CastListItem.tsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import { GoPerson } from "react-icons/go";
+
+type CastListItemProps = {
+  className: string;
+  id: number;
+  profilePath: string;
+  _name: string;
+  character: string;
+};
+
+const CastListItem = ({
+  className,
+  id,
+  profilePath,
+  _name,
+  character,
+}: CastListItemProps) => {
+  return (
+    <div className={className}>
+      <Link to={`/person/${id}`}>
+        <li className="list-item">
+          {profilePath !== null ? (
+            <img
+              className="profile"
+              src={`https://image.tmdb.org/t/p/w500${profilePath}`}
+              alt={_name}
+            />
+          ) : (
+            <GoPerson className="person-icon" />
+          )}
+          <div className="name-wrapper">
+            <h4 className="name">{_name}</h4>
+            <h4 className="role">{character}</h4>
+          </div>
+        </li>
+      </Link>
+    </div>
+  );
+};
+
+export default CastListItem;
