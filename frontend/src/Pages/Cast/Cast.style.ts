ab43df9994457c01b3410165a533d68fc3e898dc
Daniel De Lima <danielkcode@gmail.com>
2022-05-06 14:24:50 -0500

    refactored media queries

--- frontend/src/Pages/Cast/Cast.style.ts
+++ frontend/src/Pages/Cast/Cast.style.ts
@@ -10,18 +10,30 @@ const StyledCast = styled(Cast)`
     font-family: "Inter", sans-serif;
     font-size: 1.7rem;
     font-weight: 500;
-    color: rgba(16, 16, 16, 0.9);
+    color: rgba(255, 255, 255, 0.9);
     margin-block: 0.5rem;
   }
 
   /* 1024px */
   @media only screen and (min-width: 64em) {
-    width: 100%;
+    width: 70%;
+    margin-left: 15rem;
   }
 
-  /* 1295px */
-  @media only screen and (min-width: 80em) {
-    width: 50%;
+  /* 1515px */
+  @media only screen and (min-width: 94.688em) {
+    width: 60%;
+    padding-inline: 2.2rem;
+  }
+
+  /*1700px*/
+  @media only screen and (min-width: 106.25em) {
+    padding-inline: 3.5rem;
+  }
+
+  /*1800px*/
+  @media only screen and (min-width: 112.5em) {
+    padding-inline: 5.6rem;
   }
 `;
 
