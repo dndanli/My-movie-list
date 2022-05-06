58a465cb7c56863378559dc5d0a4ab2a38b1ea8e
Daniel De Lima <danielkcode@gmail.com>
2022-05-06 14:23:06 -0500

    added media queries

--- frontend/src/Pages/Home/Home.style.ts
+++ frontend/src/Pages/Home/Home.style.ts
@@ -59,5 +59,122 @@ const StyledHome = styled(Home)`
       font-weight: 500;
     }
   }
+
+  /* 635px */
+  @media only screen and (min-width: 39.6em) {
+    padding-inline: 3rem;
+    .main-panel {
+      .title {
+        font-size: 1.4rem;
+      }
+    }
+  }
+
+  /* 768px */
+  @media only screen and (min-width: 48em) {
+    padding-inline: 3.3rem;
+    .main-panel {
+      .watch-btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    }
+  }
+
+  /* 800px */
+  @media only screen and (min-width: 50em) {
+    padding-inline: 4rem;
+    .main-panel {
+      .title {
+        font-size: 1.7rem;
+      }
+      .play-icon {
+        font-size: 1.7rem;
+      }
+    }
+  }
+
+  /* 900px */
+  @media only screen and (min-width: 56.25em) {
+    padding-inline: 6rem;
+  }
+
+  /* 1024px */
+  @media only screen and (min-width: 64em) {
+    width: 70%;
+    padding-top: 1.3rem;
+    margin-left: 11rem;
+    .main-panel {
+      .watch-btn {
+        font-size: 1.2em;
+      }
+      .wrapper {
+        width: 100%;
+      }
+      .title {
+        font-size: 2rem;
+      }
+    }
+
+    .card {
+      .wrapper {
+        width: 11rem;
+      }
+    }
+
+    .hero {
+      #hero-heading {
+        font-size: 2.3rem;
+      }
+      .hero-heading-2 {
+        font-size: 1.7rem;
+      }
+    }
+
+    #popular-heading,
+    #upcoming-heading {
+      font-size: 2rem;
+    }
+
+    .section {
+      #trailers-heading {
+        font-size: 2rem;
+      }
+    }
+
+    /* 1295px */
+    @media only screen and (min-width: 80em) {
+      width: 70%;
+      .hero {
+        min-height: 20rem;
+      }
+    }
+
+    /*1515px*/
+    @media only screen and (min-width:94.688em){
+      width: 60%;
+    }
+
+    /* 1575px */
+    @media only screen and (min-width: 98em) {
+      padding-inline:7rem;
+      .search-wrapper {
+        display: none;
+      }
+    }
+
+    /*1700px*/
+    @media only screen and (min-width:106.25em){
+      padding-inline:8rem;
+
+    }
+
+    /*1700px*/
+    @media only screen and (min-width:112.5em){
+      padding-inline:10rem;
+    }
+
+  }
 `;
 export default StyledHome;
