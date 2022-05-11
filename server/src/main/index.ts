361dff7f66ea066960466f2a9a105ce5d68f5976
Daniel De Lima <danielkcode@gmail.com>
2022-05-11 16:25:58 -0500

    moved main

---
+++ server/src/main/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+
+// --- api v1 routes
+import popularRoutes from "../api/v1/routes/popular";
+import trendingRoutes from "../api/v1/routes/trending";
+import trailerRoutes from "../api/v1/routes/trailer";
+import detailRoutes from "../api/v1/routes/detail";
+import upcomingRoutes from "../api/v1/routes/upcoming";
+import searchRoutes from "../api/v1/routes/search";
+import personRoutes from "../api/v1/routes/person";
+import topRatedRoutes from "../api/v1/routes/topRated";
+
+// --- user feature routes
+import userRoutes from "../user/routes/user";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/popular", popularRoutes);
+app.use("/trending", trendingRoutes);
+app.use("/getIdForTrailer", trailerRoutes);
+app.use("/detail", detailRoutes);
+app.use("/upcoming", upcomingRoutes);
+app.use("/search", searchRoutes);
+app.use("/person", personRoutes);
+app.use("/topRated", topRatedRoutes);
+
+app.use("/user", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
