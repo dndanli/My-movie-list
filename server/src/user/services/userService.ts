7c308b176accdd7eb4a4c55b2efb99447638afc2
Daniel De Lima <danielkcode@gmail.com>
2022-05-12 21:18:52 -0500

    refactor register user

--- server/src/user/services/userService.ts
+++ server/src/user/services/userService.ts
@@ -1,23 +1,46 @@
 import { credentials } from "../../configs/postgres-config";
 import { Pool } from "pg";
+import { savePassword } from "../utils/utils";
 const pool = new Pool(credentials);
+
+
+//TODO: STORE JWT INTO HTTP ONLY COOKIE.
 
 class UserService {
   constructor() {}
+  async findUserByUsername(username: string) {
+    const client = await pool.connect();
+    try {
+      const textQuery = `
+          SELECT username FROM users WHERE username = $1
+        `;
+      return await client.query(textQuery, [username]);
+    } catch (error) {
+      console.log(`there was an error ${error}`);
+    } finally {
+      client.release();
+    }
+  }
   async registerUser(user: any) {
     const client = await pool.connect();
-
     try {
-      await client.query("BEGIN");
-      const textQuery = `
-          INSERT INTO users (username, email)
-          VALUES ($1, $2)
-      `;
-      const values = [user.username, user.email];
-      await client.query(textQuery, values);
-
-      await client.query("COMMIT");
-    } catch (error) { 
+      const res = await this.findUserByUsername(user.username);
+      if (res !== undefined) {
+        if (res.rowCount === 0) {
+          await client.query("BEGIN");
+          const textQuery = `
+              INSERT INTO users (username, email, password)
+              VALUES ($1, $2, $3)
+              `;
+          const hashedPassword = savePassword(user.password);
+          const values = [user.username, user.email, hashedPassword];
+          await client.query(textQuery, values);
+          await client.query("COMMIT");
+        } else {
+          console.log("username is not available");
+        }
+      }
+    } catch (error) {
       console.log(`there was an error ${error}`);
       await client.query("ROLLBACK");
     } finally {
