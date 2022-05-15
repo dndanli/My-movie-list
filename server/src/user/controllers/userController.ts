e734b465ce3d7c626eece1844b854ad6953f0087
Daniel De Lima <danielkcode@gmail.com>
2022-05-15 17:47:30 -0500

    updated controller

--- server/src/user/controllers/userController.ts
+++ server/src/user/controllers/userController.ts
@@ -1,15 +1,23 @@
-import {UserService} from "../services/userService";
+import { UserService } from "../services/userService";
 
-class UserController{
-  userService:UserService;
+class UserController {
+  userService: UserService;
 
-  constructor(userService:UserService){
+  constructor(userService: UserService) {
     this.userService = userService;
   }
 
-  async registerUser(user:any){
+  async registerUser(user: any) {
     return this.userService.registerUser(user);
+  }
+
+  async findUserByUsername(username: string) {
+    return this.userService.findUserByUsername(username);
+  }
+
+  async findPasswordByUsername(username: string) {
+    return this.userService.findPasswordByUsername(username);
   }
 }
 
-export {UserController}
+export { UserController };
