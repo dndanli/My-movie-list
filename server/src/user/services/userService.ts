d5b2602299d4c747fd0323f7a4ec431f35b9b4df
Daniel De Lima <danielkcode@gmail.com>
2022-05-15 17:47:12 -0500

    added findPasswordByUsername function

--- server/src/user/services/userService.ts
+++ server/src/user/services/userService.ts
@@ -1,8 +1,7 @@
 import { credentials } from "../../configs/postgres-config";
 import { Pool } from "pg";
-import { savePassword } from "../utils/utils";
+import { hashPassword } from "../utils/utils";
 const pool = new Pool(credentials);
-
 
 //TODO: STORE JWT INTO HTTP ONLY COOKIE.
 
@@ -21,23 +20,44 @@ class UserService {
       client.release();
     }
   }
+
+  async findPasswordByUsername(username: string) {
+    const client = await pool.connect();
+    console.log(`finding password for${username}`);
+    try {
+      const textQuery = `
+          SELECT password FROM users WHERE username = $1
+        `;
+      const res = await client.query(textQuery, [username]);
+      return res.rows[0].password;
+    } catch (error) {
+      console.log(`there was an error ${error}`);
+    } finally {
+      client.release();
+    }
+  }
+
   async registerUser(user: any) {
     const client = await pool.connect();
     try {
       const res = await this.findUserByUsername(user.username);
+      console.log(res);
       if (res !== undefined) {
         if (res.rowCount === 0) {
           await client.query("BEGIN");
           const textQuery = `
               INSERT INTO users (username, email, password)
               VALUES ($1, $2, $3)
+              RETURNING username
               `;
-          const hashedPassword = savePassword(user.password);
+          const hashedPassword = hashPassword(user.password);
           const values = [user.username, user.email, hashedPassword];
-          await client.query(textQuery, values);
+          const transaction = await client.query(textQuery, values);
           await client.query("COMMIT");
+          return transaction;
         } else {
           console.log("username is not available");
+          return;
         }
       }
     } catch (error) {
