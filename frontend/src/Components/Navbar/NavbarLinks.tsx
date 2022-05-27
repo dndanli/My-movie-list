c10e88a52ebb64b15879862f0c52ad656ea536d3
Daniel De Lima <danielkcode@gmail.com>
2022-05-27 16:31:27 -0500

    added profile and logout link

--- frontend/src/Components/Navbar/NavbarLinks.tsx
+++ frontend/src/Components/Navbar/NavbarLinks.tsx
@@ -1,7 +1,8 @@
-import { MdMovie, MdRateReview } from "react-icons/md";
+import { MdMovie, MdRateReview, MdLogout } from "react-icons/md";
 import { BiCube } from "react-icons/bi";
 import { IoIosLogIn } from "react-icons/io";
 import { AiOutlineUserAdd } from "react-icons/ai";
+import { IoPersonCircle } from "react-icons/io5";
 
 export const NavbarLinks: {
   id: string;
@@ -38,5 +39,17 @@ export const NavbarLinks: {
     linkIcon: <AiOutlineUserAdd />,
     linkName: "Sign up",
     link: "/signup",
+  },
+  {
+    id: "profile-link",
+    linkIcon: <IoPersonCircle />,
+    linkName: "Profile",
+    link: "/profile",
+  },
+  {
+    id: "logout-link",
+    linkIcon: <MdLogout />,
+    linkName: "Logout",
+    link: "/user/logout",
   },
 ];
