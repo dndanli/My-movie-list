4b7bd0a9003a36af5fef250f6b52e03eb88f50c2
Daniel De Lima <danielkcode@gmail.com>
2022-05-27 16:33:52 -0500

    added user profile page

--- frontend/src/Pages/Profile/UserProfile.tsx
+++ frontend/src/Pages/Profile/UserProfile.tsx
@@ -1,12 +1,54 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { UserService } from "../../Services/UserService";
+import { BsFillPersonFill } from "react-icons/bs";
+import StyledDashBoardCard from "../../Components/DashboardCard/DashboardCard.style";
+import { MdFeaturedPlayList, MdOutlineRateReview } from "react-icons/md";
+
 type UserProfileProps = {
   className: string;
 };
 
 const UserProfile = ({ className }: UserProfileProps) => {
+  const [user, setUser] = useState<any | undefined>();
+  let navigate = useNavigate();
+  useEffect(() => {
+    const userService = new UserService();
+    const fetch = async () => {
+      const userObj: any = await userService.getUserObject(navigate);
+      if (userObj.data.user !== undefined) {
+        setUser(userObj?.data.user);
+      } else {
+        setUser("");
+      }
+    };
 
-  return <div className={className}>
-    protected profile
-    </div>;
+    fetch();
+  }, [navigate]);
+  return (
+    <div className={className}>
+      <div className="person-profile-wrapper">
+        <BsFillPersonFill className="person-icon" />
+      </div>
+      <h1>Welcome {user} </h1>
+
+      <div className="profile-content">
+          <StyledDashBoardCard
+            className="dashboard-card"
+            icon={<MdFeaturedPlayList />}
+            name="Lists"
+            link="user/profile"
+            />
+
+          <StyledDashBoardCard
+            className="dashboard-card"
+            icon={<MdOutlineRateReview />}
+            name="Reviews"
+            link="/user/reviews"
+          />
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
