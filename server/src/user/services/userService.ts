aefbad214ff7d92602db56669c9966922da10e20
Daniel De Lima <danielkcode@gmail.com>
2022-05-28 12:50:51 -0500

    don't allow duplicate emails

--- server/src/user/services/userService.ts
+++ server/src/user/services/userService.ts
@@ -3,17 +3,29 @@ import { Pool } from "pg";
 import { hashPassword } from "../utils/utils";
 const pool = new Pool(credentials);
 
-//TODO: STORE JWT INTO HTTP ONLY COOKIE.
-
 class UserService {
   constructor() {}
   async findUserByUsername(username: string) {
     const client = await pool.connect();
     try {
       const textQuery = `
-          SELECT username FROM users WHERE username = $1
+          SELECT username, email FROM users WHERE username = $1
         `;
       return await client.query(textQuery, [username]);
+    } catch (error) {
+      console.log(`there was an error ${error}`);
+    } finally {
+      client.release();
+    }
+  }
+
+  async findUserByEmail(email: string) {
+    const client = await pool.connect();
+    try {
+      const textQuery = `
+          SELECT email FROM users WHERE email = $1
+        `;
+      return await client.query(textQuery, [email]);
     } catch (error) {
       console.log(`there was an error ${error}`);
     } finally {
@@ -39,21 +51,26 @@ class UserService {
   async registerUser(user: any) {
     const client = await pool.connect();
     try {
-      const res = await this.findUserByUsername(user.username);
-      console.log(res);
-      if (res !== undefined) {
-        if (res.rowCount === 0) {
-          await client.query("BEGIN");
-          const textQuery = `
-              INSERT INTO users (username, email, password)
-              VALUES ($1, $2, $3)
-              RETURNING username
-              `;
-          const hashedPassword = hashPassword(user.password);
-          const values = [user.username, user.email, hashedPassword];
-          const transaction = await client.query(textQuery, values);
-          await client.query("COMMIT");
-          return transaction;
+      const findUserRes = await this.findUserByUsername(user.username);
+      const findUserEmail = await this.findUserByEmail(user.email);
+
+      if (findUserRes !== undefined && findUserEmail !== undefined) {
+        if (findUserRes.rowCount === 0) {
+          if (findUserEmail?.rowCount === 0) {
+            await client.query("BEGIN");
+            const textQuery = `
+                INSERT INTO users (username, email, password)
+                VALUES ($1, $2, $3)
+                RETURNING username
+                `;
+            const hashedPassword = hashPassword(user.password);
+            const values = [user.username, user.email, hashedPassword];
+            const transaction = await client.query(textQuery, values);
+            await client.query("COMMIT");
+            return transaction;
+          } else {
+            console.log("this email is aleady registered with another account");
+          }
         } else {
           console.log("username is not available");
           return;
