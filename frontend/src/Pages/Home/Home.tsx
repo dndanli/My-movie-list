70d69f4b72b3a5370adf343a163d646e7006088a
Daniel De Lima <danielkcode@gmail.com>
2022-06-02 22:56:51 -0500

    passed child to parent prop and added popup

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -6,16 +6,16 @@ import { useEffect, useState } from "react";
 
 import StyledCarousel from "../../Components/Carousel/Carousel.style";
 import StyledMainPanel from "../../Components/MainPanel/MainPanel.style";
+import StyledListPopup from "../../Components/ListPopup/StyledListPopup";
 
 type Homeprops = {
   className: string;
 };
-
 // TODO: cache api data
-
 const Home = ({ className }: Homeprops) => {
   const [popularMovies, setPopularMovies] = useState<any>([]);
   const [upcoming, setUpcoming] = useState<any>([]);
+  const [currentAdded, setCurrentAdded] = useState<any>();
 
   useEffect(() => {
     let isMounted = true;
@@ -33,6 +33,16 @@ const Home = ({ className }: Homeprops) => {
     };
   }, []);
 
+  const childToParent = (cardData: any) => {
+    document.body.style.overflow = "hidden"
+    setCurrentAdded(cardData);   
+  };
+
+  const togglePopup = () =>{
+    document.body.style.overflow = ""
+    setCurrentAdded(undefined);   
+  }
+
   return (
     <div className={className}>
       <div className="home-wrapper">
@@ -45,13 +55,24 @@ const Home = ({ className }: Homeprops) => {
           slides={popularMovies}
           multiPages={true}
           heading={"Popular"}
+          childToParent={childToParent}
         />
+        {currentAdded !== undefined? (          
+          <div className="popup-wrapper">
+            <StyledListPopup
+              className="styled-list-popup"
+              cardData={currentAdded}
+              togglePopup={togglePopup}
+            />
+          </div>
+        ) : null}
         <StyledSection className="section" />
         <StyledCarousel
           className="embla"
           slides={upcoming}
           multiPages={false}
           heading={"Upcoming movies"}
+          childToParent={childToParent}
         />
       </div>
     </div>
