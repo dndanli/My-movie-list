20e5ac8c30c38a4a15804fa7f8dba5d79f635372
Daniel De Lima <danielkcode@gmail.com>
2022-06-04 17:34:39 -0500

    moved list controller functions into its own folder

--- server/src/user/controllers/userController.ts
+++ server/src/user/controllers/userController.ts
@@ -23,24 +23,8 @@ class UserController {
     return this.userService.findPasswordByUsername(username);
   }
 
-  async checkIfUserHasList(username:string, listType:string){
-    return this.userService.checkIfUserHasList(username, listType);
-  }
-
-  async getUserId(username:string){
+  async getUserId(username: string) {
     return this.userService.getUserId(username);
-  }
-
-  async getListId(userId:number, listType:string){
-    return this.userService.getListId(userId, listType);
-  }
-
-  async createNewList(listDetails:any){
-    return this.userService.createNewList(listDetails);
-  }
-
-  async createNewItem(itemObj:any){
-    return this.userService.createNewItem(itemObj);
   }
 }
 
