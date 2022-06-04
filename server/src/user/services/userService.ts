ffc2a818320f249eff1fb2471edb0b59e3629ca0
Daniel De Lima <danielkcode@gmail.com>
2022-06-04 17:34:19 -0500

    moved list functions into its own folder

--- server/src/user/services/userService.ts
+++ server/src/user/services/userService.ts
@@ -1,10 +1,14 @@
-import { credentials } from "../../configs/postgres-config";
-import { Pool } from "pg";
+import { pool } from "../../configs/postgres-config";
 import { hashPassword } from "../utils/utils";
-const pool = new Pool(credentials);
 
 class UserService {
   constructor() {}
+
+  /**
+   *
+   * @param username string
+   * @returns user result query
+   */
   async findUserByUsername(username: string) {
     const client = await pool.connect();
     try {
@@ -19,6 +23,11 @@ class UserService {
     }
   }
 
+  /**
+   *
+   * @param email string
+   * @returns user email
+   */
   async findUserByEmail(email: string) {
     const client = await pool.connect();
     try {
@@ -33,6 +42,11 @@ class UserService {
     }
   }
 
+  /**
+   *
+   * @param username string
+   * @returns hashed user password
+   */
   async findPasswordByUsername(username: string) {
     const client = await pool.connect();
     try {
@@ -48,6 +62,11 @@ class UserService {
     }
   }
 
+  /**
+   *
+   * @param username string
+   * @returns id for user with given username
+   */
   async getUserId(username: string) {
     const client = await pool.connect();
     try {
@@ -61,84 +80,11 @@ class UserService {
     }
   }
 
-  async checkIfUserHasList(username: string, listType:string) {
-    const client = await pool.connect();
-    try {
-      const userId = await this.getUserId(username);
-      const textQuery = `SELECT * FROM list WHERE user_Id = $1 AND list_type = $2`;
-
-      const res = await client.query(textQuery, [userId, listType]);
-      if (res.rows.length === 0) {
-        return false;
-      }
-      return true;
-    } catch (error) {
-      console.log(`there was an error ${error}`);
-    } finally {
-      client.release();
-    }
-  }
-  async createNewList(listDetails: any) {
-    const client = await pool.connect();
-    try {
-      await client.query("BEGIN");
-      const textQuery = `INSERT INTO list (list_type, user_id, private)
-      VALUES ($1, $2, $3)`;
-      await client.query(textQuery, [
-        listDetails.status,
-        listDetails.userId,
-        true,
-      ]);
-      await client.query("COMMIT");
-      return { sucess: true };
-    } catch (error) {
-      console.log(`there was an error ${error}`);
-      await client.query("ROLLBACK");
-      return { sucess: false };
-    } finally {
-      client.release();
-    }
-  }
-  async getListId(userId: number, listType:string) {
-    const client = await pool.connect();
-    try {
-      const textQuery = `SELECT id FROM list WHERE user_id = $1 AND list_type = $2`;
-      const res = await client.query(textQuery, [userId, listType]);
-      return res.rows[0].id;
-    } catch (error) {
-      console.log(`there was an error ${error}`);
-      await client.query("ROLLBACK");
-    } finally {
-      client.release();
-    }
-  }
-  async createNewItem(itemObj: any) {
-    const client = await pool.connect();
-    try {
-      await client.query("BEGIN");
-      const textQuery = `INSERT INTO item (api_id, list_id, media_type, title, rating, notes)
-      VALUES ($1, $2, $3, $4, $5, $6)`;
-      await client.query(textQuery, [
-        itemObj.apiId,
-        itemObj.listId,
-        itemObj.mediaType,
-        itemObj.title,
-        itemObj.rating,
-        itemObj.notes,
-      ]);
-      
-
-      await client.query("COMMIT");
-      return { sucess: true };
-    } catch (error) {
-      console.log(`there was an error: ${error}`);
-      await client.query("ROLLBACK");
-      return { sucess: false };
-    } finally {
-      client.release();
-    }
-  }
-
+  /**
+   *
+   * @param user user object with username and email
+   * @returns  QueryResult<any> transaction
+   */
   async registerUser(user: any) {
     const client = await pool.connect();
     try {
