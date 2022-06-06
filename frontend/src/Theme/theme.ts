1109ecf198c926380211c8acab80a6a1e34fcf37
Daniel De Lima <danielkcode@gmail.com>
2022-06-06 16:02:45 -0500

    added theme color file

---
+++ frontend/src/Theme/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colorDark: "#1a1b1f",
+  colorBlue: "#0d81ec",
+  colorAccentPrimary: "#34B3F1",
+  colorWhite: "rgba(255, 255, 255, 0.9)",
+  colorBlack: "rgb(51, 51, 51)",
+  colorWhiteLighter: "rgba(255, 255, 255, 0.3)",
+  colorGrey: "#a7a6ac",
+  colorGreyDarker:"rgba(33, 35, 43,1)",
+  colorRed: "#FD0054",
+  colorPink:"#f82585"
+};
