2bc6e235010e0f96dec72484fd6e6560b76a7144
Daniel De Lima <danielkcode@gmail.com>
2022-06-05 19:32:24 -0500

    changed grid layout

--- frontend/src/Components/Layout/TruncatedResult.style.ts
+++ frontend/src/Components/Layout/TruncatedResult.style.ts
@@ -5,13 +5,24 @@ const StyledTruncatedResult = styled(TruncatedResult)`
   .content-wrapper {
     padding-inline: 1rem;
     padding-block: 1.2rem;
-    display: grid;
-    grid-column-gap: 3%;
-    grid-row-gap: 2rem;
-    grid-template-columns: repeat(5, 0fr);
-    width: 90%;
-    overflow-x: auto;
-    margin-bottom: 1.5rem;
+  --grid-layout-gap: 1.5rem;
+  --grid-column-count: 6;
+  --grid-item--min-width: 9.5rem;
+
+  --gap-count: calc(var(--grid-column-count) - 1);
+  --total-gap-width: calc(var(--gap-count) * var(--grid-layout-gap));
+
+  --grid-item--max-width: calc(
+    (100% - var(--total-gap-width)) / var(--grid-column-count)
+  );
+
+  display: grid;
+  grid-template-columns: repeat(
+    auto-fill,
+    minmax(max(var(--grid-item--min-width), var(--grid-item--max-width)), 1fr)
+  );
+  grid-gap: var(--grid-layout-gap);
+  grid-row-gap: 2.5rem;
   }
 
   .truncate-wrapper {
